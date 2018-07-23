e4dcc43fc574952a29cb8b5e622377b4e6791657
Fabio Mangione <fabio.mangione@gmail.com>
2018-07-23 13:15:43 +0100

    initial commit

---
+++ src/components/Question.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class Question extends Component {
+    render() {
+        return (
+            <div className="Question">
+                <h1>is this a question?</h1>
+            </div>
+        );
+    }
+}
+
+export default Question;
