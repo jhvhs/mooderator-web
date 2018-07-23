542e1e8072f81f687b47aa77f94eb8c10bbba8e7
Fabio Mangione <fabio.mangione@gmail.com>
2018-07-23 13:39:41 +0100

    Add main components to landing page

--- src/App.js
+++ src/App.js
@@ -1,18 +1,21 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import './App.css';
+import Question from "./components/Question";
+import FancyButton from "./components/FancyButton";
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <h1 className="App-title">Mooderator</h1>
-        </header>
-
-
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div className="App">
+                <header className="App-header">
+                    <h1 className="App-title">Mooderator</h1>
+                </header>
+                <Question/>
+                <FancyButton/>
+                <FancyButton/>
+            </div>
+        );
+    }
 }
 
 export default App;
--- src/App.test.js
+++ src/App.test.js
@@ -1,7 +1,12 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 import App from './App';
+import Question from './components/Question';
+import FancyButton from "./components/FancyButton";
 
 it('renders without crashing', () => {
-    shallow(<App />);
+    const wrapper = shallow(<App />);
+
+    expect(wrapper.contains(<Question />)).toEqual(true);
+    expect(wrapper.find(FancyButton).length).toEqual(2);
 });
