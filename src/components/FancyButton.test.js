2dfbc74c2dd7af222fa8d6e3ab7f6ec828e0c63b
Fabio Mangione <fabio.mangione@gmail.com>
2018-08-01 17:48:14 +0100

    Refactor name in test

--- src/components/FancyButton.test.js
+++ src/components/FancyButton.test.js
@@ -4,8 +4,8 @@ import FancyButton from "./FancyButton";
 
 it('renders without crashing', () => {
     const wrapper = shallow(<FancyButton label={"I'm a fancy button"}/>);
-    const welcome = <button>I'm a fancy button</button>;
-    expect(wrapper.contains(welcome)).toEqual(true);
+    const content = <button>I'm a fancy button</button>;
+    expect(wrapper.contains(content)).toEqual(true);
 });
 
 it('should invoke function when clicked', (done) => {
