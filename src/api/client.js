cff85792a0c3239458f4e2b910b6f6b7d3e761be
Fabio Mangione <fabio.mangione@gmail.com>
2018-08-08 12:58:43 +0100

    Add dev dependencies
    - concurrently
    - json-server

--- src/api/client.js
+++ src/api/client.js
@@ -1,6 +1,6 @@
 // TODO change the url to be something else or a variable
 function fetchData() {
-    return fetch('https://jsonplaceholder.typicode.com/todos/1').then((result) => {
+    return fetch('http://localhost:8080/questions?latest').then((result) => {
         return result.json();
     });
 }
