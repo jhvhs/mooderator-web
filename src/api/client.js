24b16a4be2205a2ab6724fd15ab67103642c4654
Fabio Mangione <fabio.mangione@gmail.com>
2018-08-09 22:31:16 +0100

    Remove a useless TODO

--- src/api/client.js
+++ src/api/client.js
@@ -1,4 +1,3 @@
-// TODO change the url to be something else or a variable
 function fetchData() {
     return fetch('http://localhost:8080/questions?latest').then((result) => {
         return result.json();
