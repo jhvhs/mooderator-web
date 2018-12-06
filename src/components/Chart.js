6987e02308451636b9782f84945eaa062a80122f
Fabio Mangione <fabio.mangione@gmail.com>
2018-12-06 15:44:46 +0000

    Showing a chart for any answer

--- src/components/Chart.js
+++ src/components/Chart.js
@@ -3,53 +3,60 @@ import {CartesianGrid, Legend, Line, LineChart, Tooltip, XAxis, YAxis} from 'rec
 import _ from 'lodash';
 
 
+const SO_SO = "#fec621";
+const BAD = "#ea4335";
+const GOOD = "#51c0b1";
+
 class Chart extends Component {
     render() {
-
         const data = this.props.dailyStats;
         const series = this.groupData(data);
 
-        const SO_SO = "yellow";
-        const BAD = "red";
-        const GOOD = "green";
         const colors = [GOOD, BAD, SO_SO];
 
         return (
             <div className="Chart">
                 <p>{data ? data[0].question : ''}</p>
-                <div className='line-chart-wrapper'>
-                    <LineChart width={1000} height={250} margin={{top: 5, right: 30, left: 20, bottom: 5}}>
-                        <CartesianGrid strokeDasharray="3 3"/>
-                        <XAxis dataKey="day" type={"category"} padding={{left: 30, right: 30}} allowDuplicatedCategory={false}/>
-                        <YAxis/>
-                        <Tooltip/>
-                        <Legend/>
+                {series.map((s, index) => (
+                    <div key={`chart-${index}-`} className='line-chart-wrapper'>
+                    <LineChart width={700} height={250} margin={{top: 5, right: 20, left: 20, bottom: 5}}>
+                            <CartesianGrid strokeDasharray="3 3"/>
+                            <XAxis dataKey="day" type={"category"} padding={{left: 30, right: 30}}
+                                   allowDuplicatedCategory={true}/>
+                            <YAxis/>
+                            <Tooltip/>
+                            <Legend/>
 
-                        {series.map((s, index) => (
                             <Line dataKey="results"
-                                  data={s.data} name={s.name}
+                                  data={s.data}
+                                  name={s.name}
                                   key={s.name}
                                   stroke={colors[index]}
-                                  activeDot={{r: 4}}/>
-                        ))}
-                    </LineChart>
-                </div>
+                                  activeDot={{r: 3}}/>
+
+                        </LineChart>
+                        <br/>
+                        <br/>
+                    </div>
+                ))}
             </div>
         );
     }
 
     groupData(data) {
         const groupedData = _.mapValues(_.groupBy(data, q => q.answer));
-        const series = [];
 
+
+        const series = [];
         Object.keys(groupedData).forEach((group) => {
             series.push({
-                    name: group,
-                    data: groupedData[group]
-                });
+                name: group,
+                data: groupedData[group]
+            });
         });
 
-        return series;
+        return series
+        // return _.sortBy(series, (serie)=> serie.data.length, ['asc']);
     }
 }
 
